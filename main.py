8f0c577f0671770e9b6916140468442fb3d03e3f
Henrick Deschamps <henrick.deschamps@gmail.com>
2017-12-04 19:40:45 +0100

    Adding basic main file.

---
+++ main.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python2.7
+
+
+"""
+Main file.
+
+Take a list of partipants in a CSV file, create the secret santa couples, and
+send messages.
+"""
+
+
+from sys import argv, stderr
+from participant import participants_parser, participants_shuffler
+
+
+def help_message():
+    """ Help message. """
+    print >> stderr, ""
+    print >> stderr, argv[0], "<participant_file>.csv"
+    print >> stderr, ""
+    print >> stderr, "\twhere participant file contains lines like this:"
+    print >> stderr, "\t\t<firstname> <name>, <mail@mail.mail>."
+    print >> stderr, ""
+
+
+def main():
+    """ Main function. """
+    if len(argv) <= 1:
+        raise AssertionError(
+            'Error: A CSV file containing participants is needed.')
+
+    if len(argv) > 2:
+        raise AssertionError('Error: A single CSV file is needed.')
+
+    csv_file = argv[1]
+    if csv_file[-4:] != ".csv":
+        raise AssertionError('Error: The argument might not be a csv file.')
+
+    participants = participants_parser(csv_file)
+    _ = participants_shuffler(participants)
+
+if __name__ == '__main__':
+    try:
+        main()
+    except AssertionError, err:
+        print >> stderr, str(err)
+        help_message()
